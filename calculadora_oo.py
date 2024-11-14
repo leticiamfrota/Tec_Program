2935314f89a98f3ffa22d35b9dd183403c400061
leticiamfrota <leticiafrotamesquita@alu.ufc.br>
2024-11-14 13:23:04 -0300

    poo

---
+++ calculadora_oo.py
@@ -0,0 +1,9 @@
+class Calculadora:
+    def soma(self, operando_a, operando_b):
+        return (operando_a + operando_b)
+    def subtracao(self, operando_a, operando_b):
+        return (operando_a - operando_b)
+    def multiplicacao(self, operando_a, operando_b):
+        return (operando_a * operando_b)
+    def divisao(self, operando_a, operando_b):
+        return (operando_a / operando_b)
---
+++ testa_calculadora_oo.py
@@ -0,0 +1,10 @@
+from calculadora_oo import Calculadora
+
+def teste():
+    calc = Calculadora()
+    print(calc.soma(2,1))
+    print(calc.subtracao(5,3))
+    print(calc.multiplicacao(4,2))
+    print(calc.divisao(6,2))
+
+teste()
